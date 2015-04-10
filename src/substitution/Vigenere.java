87918e21e038e181cba248e88eee39364dfdec14
SAJKIEWICZ Laszlo <sajkiewl@hostname.iut2.upmf-grenoble.fr>
2015-04-10 11:17:09 +0200

    I modified the Vigenere.java

--- src/substitution/Vigenere.java
+++ src/substitution/Vigenere.java
@@ -1,23 +1,40 @@
 package substitution;
 
+import java.util.Scanner;
+
 /**
  *
  */
-public class Vigenere extends PolyAlphabetic{
+public class Vigenere {
+    private String key;
+    
+  
+ 
+   public static String encrypt(String text, final String key) {
+        String res = "";
+        text = text.toUpperCase();
+        for (int i = 0, j = 0; i < text.length(); i++) {
+            char c = text.charAt(i);
+            if (c < 'A' || c > 'Z') continue;
+            res += (char)((c + key.charAt(j) - 2 * 'A') % 26 + 'A');
+            j = ++j % key.length();
+        }
+        return res;
+    }
+ 
+   public static String decrypt(String text, final String key) {
+        String res = "";
+        text = text.toUpperCase();
+        for (int i = 0, j = 0; i < text.length(); i++) {
+            char c = text.charAt(i);
+            if (c < 'A' || c > 'Z') continue;
+            res += (char)((c - key.charAt(j) + 26) % 26 + 'A');
+            j = ++j % key.length();
+        }
+        return res;
+    }
 
-    public Vigenere(String text) {
-        super(text);
+    public String getKey() {
+        return key;
     }
-    
-    /**
-     * 
-     * @param key the key you use to cipher the text
-     * @return ciphertext
-     */
-    public String cipher(String key){
-        this.setKey(key);
-        this.setCipherText("");
-        return this.getCipherText();
-    }
-    
 }
